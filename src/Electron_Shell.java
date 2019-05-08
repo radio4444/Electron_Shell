22a2644b7aa3dd9e7522fa4258653bc644c37e7f
radio4444 <radio4444@users.noreply.github.com>
2019-05-08 17:37:33 -0400

    Initial commit

---
+++ src/Electron_Shell.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Electron_Shell {
+	public static void main (String args[]) {
+		System.out.println("Welcome! This is a compute number of electron in shell");
+		System.out.println("In this program, the user would enter one of the shells: K,M,N,O,P,Q;" +
+				"and learn how many of electrons in 'X'(which is the user's input) shell hold.");
+		Scanner input = new Scanner(System.in);
+		
+	}
+}
